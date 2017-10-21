3ac76c0990035b4355a25e70e577c10be2120b62
Ashley Coleman <ashley78@ksu.edu>
2017-10-21 11:35:25 -0500

    Add basic madlib enpoind

--- routes/api.php
+++ routes/api.php
@@ -18,3 +18,18 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::post('/create/party', 'ApiController@CreateParty');
+
+Route::get('/madlib', function() {
+    return response()->json([
+        'lines' => [
+            'Be kind to your Dog-footed Carrots', 
+            'For a duck may be somebody`s Jim Carrey,',
+            'Be kind to your Carrots in Topeka',
+            'Where the weather is always Tiny.',
+            'You may think that this is the Nigel Thornberry,',
+            'Well it is.'
+        ]
+    ]);
+});
+
+
